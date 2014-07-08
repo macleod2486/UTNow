cdd5c8d64b1af7566ee19a979c6bdedb86bffe46
Manuel Gonzales <macleod2486@gmail.com>
2014-07-08 00:21:32 -0500

    Set broadcast reciever to start service upon boot

--- src/com/macleod2486/utnow/UTBroadcast.java
+++ src/com/macleod2486/utnow/UTBroadcast.java
@@ -21,9 +21,13 @@
 */
 package com.macleod2486.utnow;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class UTBroadcast extends BroadcastReceiver 
@@ -32,11 +36,37 @@ public class UTBroadcast extends BroadcastReceiver
 	@Override
 	public void onReceive(Context arg0, Intent arg1) 
 	{
-		Log.i("UTBroadcast","Broadcast recieved");
+		SharedPreferences shared = PreferenceManager.getDefaultSharedPreferences(arg0.getApplicationContext());
 		
-		//Starting the news update class
-		Intent newsUpdate = new Intent(arg0,NewsUpdate.class);
-		arg0.startService(newsUpdate); 
+		if(arg1.toString().contains(Intent.ACTION_BOOT_COMPLETED) && shared.getBoolean("notification", false))
+		{
+			
+			//one second * 60 seconds in a minute * 5
+			long fiveMinutes = 1000*60*5;
+			
+			Intent service = new Intent(arg0,UTBroadcast.class);
+			PendingIntent pendingService = PendingIntent.getBroadcast(arg0, 0, service, 0);
+			AlarmManager newsUpdate = (AlarmManager)arg0.getSystemService(arg0.ALARM_SERVICE);
+			
+			//Check for the update every 5 minutes
+			newsUpdate.setRepeating(AlarmManager.RTC, System.currentTimeMillis(), fiveMinutes, pendingService);
+			
+			SharedPreferences.Editor edit = shared.edit();
+			edit.putBoolean("notifCancel", false);
+			edit.commit();
+			
+			Log.i("UTBroadcase","UTService started");
+		}
+		else
+		{
+			//Starting the news update class
+			
+			Intent newsUpdate = new Intent(arg0,NewsUpdate.class);
+			arg0.startService(newsUpdate);
+			
+			Log.i("UTBroadcast","Broadcast finished");
+		}
+		 
 	}
 
 }
